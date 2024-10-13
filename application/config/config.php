b313ae5b460db6144b8fd79c347c468afb897fee
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-13 14:54:06 +0530

    updated

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/codecanyon-grocery/';
+$config['base_url'] = 'http://localhost/Grocery-Store-Android-App-With-Admin-Panel/';
 $config['theme_admin'] = "theme/admin";
 
  $config['currency'] = 'USD';
